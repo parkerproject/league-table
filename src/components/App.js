eb9640b0442dea048d3199274180c4e425bd28f2
Parker <parkerproject@gmail.com>
2017-08-25 15:41:47 -0400

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,102 +1,30 @@
-import React, { Component } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import axios from 'axios';
-import Table from './table';
-import '../App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import Bankruptcy from './bankruptcy';
+import OutOfCourt from './out_of_court';
+import PrivateEquity from './pe';
+import MergerAquisition from './ma';
 
-const API_URL = 'https://league-table.herokuapp.com/bankruptcy';
-
-const styles = {
-  table: {
-    width: '345px',
-    float: 'left',
-    margin: '5px',
-    fontSize: '14px',
-  },
-  list: {
-    display: 'inline-block',
-    padding: '10px',
-    fontSize: 13,
-    border: '1px solid gray',
-    borderLeft: 0,
-    cursor: 'pointer',
-  },
-  ul: {
-    padding: 0,
-    borderLeft: '1px solid gray',
-  },
-  blurb: {
-    background: 'gray',
-    width: 695,
-    color: 'white',
-    fontSize: 13,
-    padding: 5,
-  },
-  main: {
-    background: 'transparent linear-gradient(to bottom, #DC1010 0%, #C61102 100%) repeat scroll 0% 0%',
-    width: 695,
-    padding: 5,
-    color: 'white',
-  },
-
-};
-
-class App extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      activeTab: 'Bankruptcy monitor',
-    };
+function App(props) {
+  if (props.type === 'br') {
+    return <Bankruptcy />;
   }
 
-  async componentWillMount() {
-    const res = await axios.get(API_URL);
-    const { tabs, results } = res.data;
-    this.setState({ tabs, results });
+  if (props.type === 'oc') {
+    return <OutOfCourt />;
   }
 
-  selectedTab(index) {
-    const tabName = this.state.tabs[index];
-    console.log(tabName);
+  if (props.type === 'pe') {
+    return <PrivateEquity />;
   }
 
-  render() {
-    if (!this.state.results) {
-      return <div>loading...</div>;
-    }
-
-    const { tabs, results } = this.state;
-    return (
-      <div style={{ fontFamily: 'arial' }}>
-        <Tabs onSelect={index => this.selectedTab(index)}>
-          <TabList style={styles.ul}>
-            {tabs.map((name, key) => <Tab key={key} style={styles.list}>{name}</Tab>)}
-          </TabList>
-          <div style={styles.main}>Bankruptcy Rankings Chart <small>First Half</small></div>
-          <div style={styles.blurb}>U.S bankruptcy cases with $25m+ in liabilites <small>First Half</small></div>
-
-          {tabs.map((result, key) => {
-            const investmentByCases = results.filter(e => e.TAB === result && e.RANKTYPE === 'CASES');
-            const investmentByVolume = results.filter(e => e.TAB === result && e.RANKTYPE === 'VOLUME');
-            return (
-              <TabPanel key={key}>
-                <div style={styles.table}>
-                  <Table title="Top Investment advisers, by dollar volume" data={investmentByVolume} type="volume" />
-                </div>
-                <div style={styles.table}>
-                  <Table title="Top Investment advisers, by no. of cases" data={investmentByCases} />
-                </div>
-              </TabPanel>
-            );
-          })}
-
-
-        </Tabs>
-      </div>
-
-    );
+  if (props.type === 'ma') {
+    return <MergerAquisition />;
   }
 }
+
+App.propTypes = {
+  type: PropTypes.string.isRequired,
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,16 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('league_table'));
+/*
+league table types
+1. bankruptcy
+2. out of court
+3. ma
+4. private equity
+*/
+
+const leagueTable = document.getElementById('league_table');
+const typeOfLeagueTabe = leagueTable.getAttribute('data-league-type');
+
+ReactDOM.render(<App type={typeOfLeagueTabe} />, leagueTable);
 registerServiceWorker();
